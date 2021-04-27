338c674634b43ea6d3dea884429f1f6610ef8b95
Ibrahim <flamexshady@gmail.com>
2021-04-27 15:59:08 -0500

    map view semi complete

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
 import CreatePop from './CreatePop.jsx';
 import MyPops from './MyPops.jsx';
+import { 
+  GoogleMap, 
+  useLoadScript,
+  Marker,
+  InfoWindow
+} from '@react-google-maps/api';
+import Map from './mapView.jsx';
+import * as merchData from './openMerch.json';
+// const MapView = withScriptjs(withGoogleMap(Map));
+
 
 const App = () => {
     const [myPops, setMyPops] = useState([
@@ -15,10 +25,19 @@ const App = () => {
         lon: 0,
       },
     ]);
+    
     return (
       <div>
+        <div style={{width: '100vw', height: '100vh'}}>
         <CreatePop myPops={myPops} setMyPops={setMyPops}/>
         <MyPops myPops={myPops} setMyPops={setMyPops}/>
+        <Map
+        loadingElement={<div style={{height: '80%' }}/>}
+        containerElement={<div style={{height: '80%' }}/>}
+        mapElement={<div style={{height: '80%' }}/>}
+        merchData={merchData}
+        />
+        </div>
       </div>
     )
 };
