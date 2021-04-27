4548dbd5daecef82dca897da1ff7251ea8b3589d
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-04-26 20:35:32 -0500

    changed popups to merchants in database schema

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+
 const app = express();
 const PORT = 8080;
 const CLIENT_PATH = path.resolve(__dirname, '../client/dist');
 app.use(express.static(CLIENT_PATH));
+//end point time
+
+app.post('/addmerchant', (req, res) => {
+  res.send('success');
+});
+
 app.listen(PORT, (() => {
   console.log(`Server listening at http://127.0.0.1:${PORT}`);
 }));
-
-//end point time
