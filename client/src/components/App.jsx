e8b59a9dd20e45ce8c18893fa195c8618a1630bf
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-04-27 18:00:22 -0500

    user can log in and log out

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,16 +7,42 @@ import MyPops from './MyPops.jsx';
 import axios from 'axios';
 
 const App = () => {
+    const [gotPops, setGotPops] = useState(false);
+    const [gotLogged, setGotLogged] = useState(false);
+    const [username, setUsername] = useState('');
+    const [isLogged, setIsLogged] = useState(false);
     const [myPops, setMyPops] = useState([]);
+    const [login, setLogin] = useState(<a href="/google"> Login </a>)
     //grab from database
     const getPops = () => {
-      axios.get('/merchants')
-        .then(response => setMyPops(response.data))
+      if (!gotPops) {
+        setGotPops(true);
+        axios.get('/merchants')
+          .then(response => setMyPops(response.data))
+      }
     }
     getPops();
+    const logged = () => {
+      if (!gotLogged) {
+        setGotLogged(true);
+        axios.get('/testing')
+          .then(results => {
+            console.log(results.data);
+            if (results.data.displayName) {
+              setIsLogged(true);
+              setUsername(data.displayName);
+              setLogin(<a href="/logout"> Logout </a>);
+            }
+          });
+      }
+    }
+    logged();
     return (
       <div>
-        <a href="/google"> Login </a>
+        {isLogged === true
+          ? (<a href="/logout"> Logout </a>)
+        : (<a href="/google"> Login </a>)
+        }
         <h1>Welcome to Pop^</h1>
         <CreatePop myPops={myPops} setMyPops={setMyPops}/>
         <MyPops myPops={myPops} setMyPops={setMyPops}/>
