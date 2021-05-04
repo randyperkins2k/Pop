0450152edeb4ec1d9705e93752e8441054c086af
Ibrahim <flamexshady@gmail.com>
2021-05-04 17:29:03 -0500

    picture feed mostly complete

--- client/src/components/YourPopups/EditPopUp/UploadPic.jsx
+++ client/src/components/YourPopups/EditPopUp/UploadPic.jsx
@@ -1,7 +1,64 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
-const UploadPic = () => {
+const UploadPic = ({ merchant }) => {
+  const [ fileInputState, setFileInputState ] = useState('');
+  const [ selectedFile, setSelectedFile ] = useState('');
+  const [ previewSource, setPreviewSource ] = useState('');
+  
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    previewFile(file);
+  }
 
+  const previewFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setPreviewSource(reader.result)
+    }
+  }
+
+  const handleSubmitFile = (e) => {
+    console.log('submitting')
+    e.preventDefault();
+    if (!previewSource) {
+      return
+    }
+    // const reader = new FileReader();
+    // reader.readAsDataURL(selectedFile);
+    uploadImage(previewSource);
+  }
+
+  const uploadImage = async (base64EncodedImage) => {
+    try {
+      const response = await axios.post(`/api/images/upload/${merchant.id}`, {image: base64EncodedImage})
+      console.log(response.data)
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <div>
+      <h1>Upload</h1>
+      <form onSubmit={handleSubmitFile}>
+        <input
+          type="file" name="image" onChange={handleFileInputChange} value={fileInputState} className="form-input"
+        ></input>
+        <button className="btn" type="submit">Upload</button>
+      </form>
+      {
+        previewSource ?
+        <div>
+          <h3>image should preview</h3>
+          <img src={previewSource} alt="chosen" style={{ height: "300px"}}/>
+        </div>
+        :
+        ''  
+      }
+    </div>
+  )
 }
 
 export default UploadPic;
