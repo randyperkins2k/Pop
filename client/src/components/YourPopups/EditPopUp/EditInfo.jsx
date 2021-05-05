5732586e46b9f26e98051dfb7314a9976416b12d
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-05-05 12:12:26 -0500

    api call from client works

--- client/src/components/YourPopups/EditPopUp/EditInfo.jsx
+++ client/src/components/YourPopups/EditPopUp/EditInfo.jsx
@@ -1,8 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const EditInfo = ({ merchant, selectMerchant, yourPopups, setYourPopups, userSubs, setUserSubs, merchData, setMerchData }) => {
+  //console.log(merchant.info);
+  const [text, setText] = useState(merchant.info);
+  const updateInfo = () => {
+    console.log(text);
+    if (text) {
+      axios.put('/api/merchant/updateinfo', {
+        id: merchant.id, info: text
+      })
+      .then(result => {
+        console.log(result.data)
+      })
+      .catch(err => console.log(err));
+    }
+  };
+
   return (
-  <div>{merchant.name}</div>
+  <div>
+    <h3>edit {`${merchant.name}'s`} info: </h3>
+    <textarea value={text} onChange={(e) => setText(e.target.value) }></textarea>
+    <button onClick={ ()=> updateInfo()}>update info</button>
+  </div>
   )
 };
 
