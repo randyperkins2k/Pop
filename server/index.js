edbf88e7bdeaa14c048a60b5312a3ae6a3d797da
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-05-05 14:05:10 -0500

    made endpoint that gets distance between to points

--- server/index.js
+++ server/index.js
@@ -151,6 +151,38 @@ app.get('/merchant/:id', (req, res) => {
     .then(data => res.send(data))
     .catch(err => res.send(err));
 });
+
+//get merchants in given radius
+app.get('/api/merchant/radius/:miles', (req, res) => {
+  Merchants.findAll({
+    where: {}
+  })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => res.send(err));
+});
+
+app.get('/distance/:lat1/:lon1/:lat2/:lon2', (req, res) => {
+  const {lat1, lon1, lat2, lon2} = req.params;
+
+  function deg2rad(deg) {
+    return deg * (Math.PI/180)
+  }
+
+  function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    return d;
+  }
+  console.log(getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2))
+  res.send('check console');
+});
+
 //add new merchant
 app.post('/api/merchant/add', (req, res) => {
   const { name, category, info, website, adminId, lat, lon } = req.body;
