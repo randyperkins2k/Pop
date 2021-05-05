bcdca4617c86f5864693e59332b7b540edafce67
Randolph Perkins <randyperkins2k@gmail.com>
2021-05-05 17:16:34 -0500

    Created dark mode theme and hook, button, etc.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -32,8 +32,11 @@ import {
   Link,
   Redirect,
 } from 'react-router-dom';
-import styled, { css } from 'styled-components'
 import { useTranslation } from 'react-i18next'
+
+// Styling Components
+import styled, { css, ThemeProvider } from 'styled-components'
+import darkTheme from '../components/Themes/darkMode.js';
 
 
 // const MapView = withScriptjs(withGoogleMap(Map));
@@ -87,6 +90,11 @@ color: #ffd1dc;
 text-align: center;
 margin-bottom: 2.6rem;
 `
+const Moon = styled.svg`
+  height: auto;
+  width: 2.5rem;
+  transition: all 0.7s linear;
+`;
 
 import axios from 'axios';
 
@@ -100,9 +108,14 @@ const App = () => {
   const [ userSubs, setUserSubs] = useState([]);
   const [yourPopups, setYourPopups] = useState([]);
   const [ currentLocMarker, setCurrentLocMarker ] = useState(null);
-  const [lVPrimary, setLVPrimary] = useState(false)
-  const [mLPrimary, setMLPrimary] = useState(true)
-  //grab from database
+  const [lVPrimary, setLVPrimary] = useState(false);
+  const [mLPrimary, setMLPrimary] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(stored === 'true' ? true : false);
+
+  // Storage for dark mode setting.
+  const stored = localStorage.getItem('isDarkMode');
+
+  // Fetches merchant data from database.
   const getPops = () => {
     axios.get('/merchants')
       .then(response => {
@@ -182,6 +195,8 @@ const App = () => {
             setYourPopups={setYourPopups}
             currentLocMarker={currentLocMarker}
             setCurrentLocMarker={setCurrentLocMarker}
+            isDarkMode={isDarkMode}
+            setIsDarkMode={setIsDarkMode}
            />}}/>
       <Route
         path="/login"
@@ -201,8 +216,8 @@ const Home = ({
   userSubs, setUserSubs, yourPopups, setYourPopups,
   currentLocMarker, setCurrentLocMarker,
   lVPrimary, setLVPrimary,
-  mLPrimary, setMLPrimary
-
+  mLPrimary, setMLPrimary,
+  isDarkMode, setIsDarkMode
 }) => {
   const { t, i18n } = useTranslation();
     return(
@@ -211,6 +226,7 @@ const Home = ({
       {/* <LogOutBtn href="/logout"> Logout </LogOutBtn> */}
 
         <div className='sidebar-view'>
+            <button><Moon xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path style={{ fill: "#F6C358" }} d="M10.719 2.082c-2.572 2.028-4.719 5.212-4.719 9.918 0 4.569 1.938 7.798 4.548 9.895-4.829-.705-8.548-4.874-8.548-9.895 0-5.08 3.808-9.288 8.719-9.918zm1.281-2.082c-6.617 0-12 5.383-12 12s5.383 12 12 12c1.894 0 3.87-.333 5.37-1.179-3.453-.613-9.37-3.367-9.37-10.821 0-7.555 6.422-10.317 9.37-10.821-1.74-.682-3.476-1.179-5.37-1.179zm0 10.999c1.437.438 2.562 1.564 2.999 3.001.44-1.437 1.565-2.562 3.001-3-1.436-.439-2.561-1.563-3.001-3-.437 1.436-1.562 2.561-2.999 2.999zm8.001.001c.958.293 1.707 1.042 2 2.001.291-.959 1.042-1.709 1.999-2.001-.957-.292-1.707-1.042-2-2-.293.958-1.042 1.708-1.999 2zm-1-9c-.437 1.437-1.563 2.562-2.998 3.001 1.438.44 2.561 1.564 3.001 3.002.437-1.438 1.563-2.563 2.996-3.002-1.433-.437-2.559-1.564-2.999-3.001z" /></Moon></button>
             <Welcome onClick={() => setSideBarDisplay(!sideBarDisplay)}>Pop^</Welcome>
             {/* <ToggleSwitch /> */}
             {
