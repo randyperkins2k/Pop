9e4543503b987a4099a8123da02521c26bd75e35
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-05-05 15:18:46 -0500

    making radius endpoint

--- server/index.js
+++ server/index.js
@@ -153,11 +153,25 @@ app.get('/merchant/:id', (req, res) => {
 });
 
 //get merchants in given radius
-app.get('/api/merchant/radius/:miles', (req, res) => {
+app.get('/api/merchant/radius/:km/:lat1/:lon1/:lat2/:lon2', (req, res) => {
   Merchants.findAll({
     where: {}
   })
     .then(data => {
+      function deg2rad(deg) {
+        return deg * (Math.PI/180)
+      }
+
+      function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = deg2rad(lat2-lat1);  // deg2rad below
+        var dLon = deg2rad(lon2-lon1);
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c; // Distance in km
+        return d;
+      }
+
       res.send(data);
     })
     .catch(err => res.send(err));
