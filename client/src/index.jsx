7303da0b1435df686133fb90b5164fdbc258fae5
Ibrahim <flamexshady@gmail.com>
2021-05-06 19:48:16 -0500

    merged

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,22 +2,7 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import GlobalStyles from './components/styles/globalStyles.js';
-<<<<<<< HEAD
 import { registerServiceWorker } from './serviceWorker'
-
-
-ReactDOM.render(
-<React.Fragment>
-<GlobalStyles/>
-<App />
-</React.Fragment>,
- document.getElementById('app'));
-
- registerServiceWorker();
-
-
-
-=======
 import './i18n.jsx';
 
 
@@ -29,4 +14,6 @@ ReactDOM.render(
 	</React.Fragment>
 </Suspense>,
  document.getElementById('app'));
->>>>>>> b07bda9e7d52c49be6b49103f62bd4906b47d967
+
+ 
+ registerServiceWorker();
