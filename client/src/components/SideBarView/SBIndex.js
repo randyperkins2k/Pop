c36305d79894e9e4e7628305c9ce5ceaddbd71ce
Randolph Perkins <randyperkins2k@gmail.com>
2021-05-10 20:25:39 -0500

    Working in sidebar.

---
+++ client/src/components/SideBarView/SBIndex.js
@@ -0,0 +1,4 @@
+
+
+export { default as Burger } from './Burger';
+export { default as Menu } from './Menu';
--- client/src/components/SideBarView/SideBar.jsx
+++ client/src/components/SideBarView/SideBar.jsx
@@ -7,7 +7,7 @@ import { useTranslation } from 'react-i18next'
 const styles = {
   main:{'margin': 0,
     'padding': 0,
-    'width': '210px',
+    'width': '250px',
     'backgroundColor': '#f1f1f1',
     'position': 'absolute',
     'opacity': '.90',
@@ -32,11 +32,28 @@ const Button = styled.button`
   width: 9em;
   display: block;
 `
-
+const SidebarDiv = styled.div`
+  margin: 0,
+  padding: 0,
+  width: 150px,
+  background-color: #f1f1f1,
+  position: absolute,
+  opacity: .90,
+  height: 100vh,
+  overflow: auto,
+  z-index: 99
+`
+const SidebarUl = styled.ul`
+  display: block,
+  color: white,
+  padding: 16px,
+  text-decoration': none
+`
 const SideBar = ({ close, setMLPrimary, setLVPrimary }) => {
 const {t} = useTranslation();
   return (
     <div style={styles.main} className='sidebar'>
+      {/*  <SidebarDiv> */}
       <ul styles={styles.links}>
         <br/>
         <Link to='/'>
@@ -77,7 +94,8 @@ const {t} = useTranslation();
             setMLPrimary(false)
             }}>{t('logoutBtn')}</a></Button>
       </ul>
-    </div>
+    {/* </SidebarDiv> */}
+     </div>
   )
 }
 
