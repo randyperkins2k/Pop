20397b2deb175e349f7c2d13854933915412829e
daniel knowles <danielknowles@daniels-MacBook-Pro.local>
2021-05-15 17:58:10 -0500

    cleaned up files

--- client/src/components/Confirmation.jsx
+++ client/src/components/Confirmation.jsx
@@ -2,15 +2,6 @@ import React , { useState } from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next'
 
-// const Create = styled.button`
-//   margin-left: -1px;
-//   background-color: white;
-//   border-style: solid;
-//   border-width: 1px;
-//   border-color: lightgray;
-//   border-top-right-radius: 6px;
-//   border-bottom-right-radius: 6px;
-// `
 
 const Confirmation = ({ text, yesContext, noContext }) => {
 const {t} = useTranslation()
