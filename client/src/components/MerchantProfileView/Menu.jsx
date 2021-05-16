b1afff0340032b9f10a0fc697d80220810950606
daniel knowles <danielknowles@daniels-MacBook-Pro.local>
2021-05-15 23:27:19 -0500

    styled menu

--- client/src/components/MerchantProfileView/Menu.jsx
+++ client/src/components/MerchantProfileView/Menu.jsx
@@ -1,6 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useTranslation } from 'react-i18next';
+import styled from 'styled-components'
+
+const H22 = styled.h1`
+
+margin-top: 15px;
+margin-bottom: 0px;
+color:#f5abc9;
+font-size: 1.5rem;
+
+`
+
+const H3 = styled.h3`
+font-family: 'Londrina Solid',cursive;
+margin-top: 3px;
+color: ${props => props.theme.mode === 'dark' ? 'rgb(224, 217, 220)' : '#000'};
+
+`
 
 const Menu = ({ merchant, selectMerchant }) => {
   const [products, setProducts] = useState([]);
@@ -17,9 +34,9 @@ const Menu = ({ merchant, selectMerchant }) => {
 
   return (
     <div>
-        <h3>{`${t("productsTxt")}:`} </h3>
+        <H22>{`${t("productsTxt")}:`} </H22>
         {products.map(product =>
-          <p>&emsp; &#8226; &nbsp;<b>{product.name}</b><small>(<i>${parseFloat(product.price).toFixed(2)}</i></small>)</p>
+          <H3>&emsp; &#8226; &nbsp;<b>{product.name}</b><small>(<i>${parseFloat(product.price).toFixed(2)}</i></small>)</H3>
         )}
     </div>
   );
